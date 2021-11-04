d174a4b0eb0ca9b4378045c26fb8b8d7e64d275c
Andres2D <andres2d1997.1206@gmail.com>
2021-11-03 20:42:38 -0500

    Add cotejo module

---
+++ src/app/pages/cotejo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CotejoRoutingModule } from './cotejo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CotejoRoutingModule
+  ]
+})
+export class CotejoModule { }
