31b9d823cec44ff457bb4ab040976aa37a7c87d5
Andres2D <andres2d1997.1206@gmail.com>
2021-11-05 20:52:22 -0500

    add reactive form to the login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  loginForm: FormGroup = this.fb.group({
+    email: ['first@mail.com', Validators.email],
+    password: ['123456']
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  login() {
+    if(this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
   }
-
 }
