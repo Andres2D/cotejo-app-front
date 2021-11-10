f3c67b91dbc213d0dc55193891e9195790f3afa2
Andres2D <andres2d1997.1206@gmail.com>
2021-11-09 20:14:23 -0500

    Add actions icons just :art: and update general styles issues

--- src/app/components/cotejo-player-preview/cotejo-player-preview.component.ts
+++ src/app/components/cotejo-player-preview/cotejo-player-preview.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cotejo-player-preview',
   templateUrl: './cotejo-player-preview.component.html',
   styleUrls: ['./cotejo-player-preview.component.scss']
 })
-export class CotejoPlayerPreviewComponent implements OnInit {
+export class CotejoPlayerPreviewComponent {
 
-  constructor() { }
+  @Input() img: string = 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Gray02&eyeType=Default&eyebrowType=Default&mouthType=Serious&skinColor=Brown';
+  @Input() update: boolean = false;
 
-  ngOnInit(): void {
+  @Output() emitUpdate = new EventEmitter();
+
+  updateImage(): void {
+    this.emitUpdate.emit();
   }
-
 }
