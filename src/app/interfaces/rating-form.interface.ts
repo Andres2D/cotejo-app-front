4c5302318685448bf7436c5934560e5099c32b5e
Andres2D <andres2d1997.1206@gmail.com>
2021-11-10 20:28:08 -0500

    update button component and set modal data

---
+++ src/app/interfaces/rating-form.interface.ts
@@ -0,0 +1,4 @@
+export interface RatingForm {
+    label: string;
+    control: string;
+}
---
+++ src/app/pages/player/player.constants.ts
@@ -0,0 +1,28 @@
+import { RatingForm } from "src/app/interfaces/rating-form.interface";
+
+export const ratingForm: RatingForm[] = [
+    {
+        label: 'Defense',
+        control: 'defense'
+    },
+    {
+        label: 'Passing',
+        control: 'passing'
+    },
+    {
+        label: 'Physical',
+        control: 'physical'
+    },
+    {
+        label: 'Dribbling',
+        control: 'dribbling'
+    },
+    {
+        label: 'Peace',
+        control: 'peace'
+    },
+    {
+        label: 'Shooting',
+        control: 'shooting'
+    }
+]
