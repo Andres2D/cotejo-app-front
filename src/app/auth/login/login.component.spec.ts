ddebc3d81602de081ddd12cd0e223a567ae12df5
Andres2D <andres2d1997.1206@gmail.com>
2021-11-24 11:25:34 -0500

    Add testing to the login component

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,10 +1,20 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TestBed, tick } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../../services/auth.service';
 import { ComponentsModule } from '../../components/components.module';
 import { LoginComponent } from './login.component';
+import { of } from 'rxjs';
+
+const fakeAuthService = {
+  login() {}
+}
+
+const mockResponse = {
+  ok: false,
+  msg: 'User invalid'
+};
 
 function setUp() {
   TestBed.configureTestingModule({
@@ -14,7 +24,12 @@ function setUp() {
       RouterTestingModule,
       HttpClientTestingModule
     ],
-    providers: [],
+    providers: [
+      {
+        provide: AuthService,
+        useValue: fakeAuthService
+      }
+    ],
     declarations: [ LoginComponent ]
   });
   const fixture = TestBed.createComponent(LoginComponent);
@@ -29,20 +44,52 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  //TODO: Complete test
-  it('should render alert error', fakeAsync(() => {
-    const {element, component, fixture} = setUp();
-    component.loginForm.setValue({
-      email: 'wrong@mail.com',
-      password: '144'
-    });
-
-    const loginBtn = element.query(By.css('.login-btn'));
-    loginBtn.triggerEventHandler('click', {button: 0});
-    tick(500);
-    component.login();
+  it('should render alert component', () => {
+    const { component, fixture, element } = setUp();
+    component.showAlert = true;
     fixture.detectChanges();
-    const alertDanger = element.query(By.css('.alert')); 
-    expect(true).toBeTruthy();
-  }));
+    const alert = element.nativeElement.querySelector('.alert');
+    expect(alert).toBeTruthy();
+  });
+
+  it('should close alert component', () => {
+    const { component, fixture, element } = setUp();
+    component.showAlert = true;
+    fixture.detectChanges();
+    let alert = element.nativeElement.querySelector('.alert');
+    expect(alert).toBeTruthy();
+    component.close();
+    fixture.detectChanges();
+    alert = element.nativeElement.querySelector('.alert');
+    console.log(component.showAlert);
+    expect(alert).not.toBeTruthy();
+  });
+
+  it('should not login when form is invalid', () => {
+    const { component, element, fixture } = setUp();
+    const comSpy = jest.spyOn(component, 'login');
+    const servSpy = jest.spyOn(fakeAuthService, 'login');
+    fixture.detectChanges();
+    component.loginForm.controls['email'].setValue('wrong');
+    component.loginForm.controls['password'].setValue('111');
+    const button = element.nativeElement.querySelector('.login-btn');
+    button.click();
+    fixture.detectChanges();
+    expect(comSpy).toHaveBeenCalled();
+    expect(servSpy).not.toHaveBeenCalled();
+  });
+
+  it('should login when form is valid', () => {
+    const { component, element, fixture } = setUp();
+    const comSpy = jest.spyOn(component, 'login');
+    const servSpy = jest.spyOn(fakeAuthService, 'login');
+    fixture.detectChanges();
+    component.loginForm.controls['email'].setValue('prueba@mail.com');
+    component.loginForm.controls['password'].setValue('111');
+    const button = element.nativeElement.querySelector('.login-btn');
+    button.click();
+    fixture.detectChanges();
+    expect(comSpy).toHaveBeenCalled();
+    expect(servSpy).toHaveBeenCalled();
+  });
 });
