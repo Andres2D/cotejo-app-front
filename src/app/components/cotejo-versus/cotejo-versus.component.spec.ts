61404e486904b7f767346a561dbecca7c9efe664
Andres2D <andres2d1997.1206@gmail.com>
2021-12-11 22:03:12 -0500

    add test to the versus component

--- src/app/components/cotejo-versus/cotejo-versus.component.spec.ts
+++ src/app/components/cotejo-versus/cotejo-versus.component.spec.ts
@@ -22,4 +22,28 @@ describe('CotejoVersusComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the init data', () => {
+    const fakeInitNames = {
+      home: 'Liverpool',
+      away: 'Chelsea'
+    };
+    component.home.name = fakeInitNames.home;
+    component.away.name = fakeInitNames.away;
+    fixture.detectChanges();
+    const home = fixture.nativeElement.querySelector('.home'); 
+    const away = fixture.nativeElement.querySelector('.away'); 
+    expect(home.textContent).toBe(fakeInitNames.home);
+    expect(away.textContent).toBe(fakeInitNames.away);
+  });
+
+  it('should emit the correct id', () => {
+    const spy = jest.spyOn(component.clickMatch, 'emit');
+    component.id = '123';
+    fixture.detectChanges();
+    const parent = fixture.nativeElement.querySelector('.fixture');
+    parent.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled();
+  });
 });
