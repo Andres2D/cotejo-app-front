81c107c9c87b477e8bfe11df56fd68821ea21087
Andres2D <andres2d1997.1206@gmail.com>
2022-02-11 20:52:38 -0500

    Add card component with back option

---
+++ src/app/components/cotejo-page-cards/cotejo-page-cards.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cotejo-page-cards',
+  templateUrl: './cotejo-page-cards.component.html',
+  styleUrls: ['./cotejo-page-cards.component.scss']
+})
+export class CotejoPageCardsComponent {
+}
