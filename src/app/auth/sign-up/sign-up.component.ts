f192250951ef1585a90838a40e9c01ff9618ad36
Andres2D <andres2d1997.1206@gmail.com>
2022-02-13 20:51:19 -0500

    Add layout for signUp page

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  disableSubmit: boolean = false;
+
+  signUpForm: FormGroup = this.fb.group({
+    name: ['Test', Validators.required],
+    email: ['first@mail.com', [Validators.email, Validators.required]],
+    nickname: ['', Validators.required],
+    dorsal: [0, Validators.required],
+    status: [''],
+    password: ['123456', Validators.required],
+    image: ['https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortWaved&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder, 
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  signUp(): void {
+
+  }
 }
