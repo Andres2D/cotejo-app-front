1eb0a0e62542e4d7d1fb20f17f1cbf2a41350751
Andres2D <andres2d1997.1206@gmail.com>
2022-02-26 02:15:12 -0500

    Add constants and interface

---
+++ src/app/constants/player-quotes.ts
@@ -0,0 +1,120 @@
+import { PlayerQuote } from '../interfaces/player-quote.interface';
+
+export const quotes: PlayerQuote[] = [
+    {
+        quote: 'El éxito no es un accidente. Es trabajo duro, perseverancia, aprendizaje, sacrificio y, sobre todo, amor por lo que estás haciendo o aprendiendo a hacer',
+        player: 'Pelé'
+    },
+    {
+        quote: 'Juego para ser feliz y la gente que valore lo que tenga que valorar. Si valoran mi trabajo, encantado, si no, no pasa nada',
+        player: 'Andrés Iniesta'
+    },
+    {
+        quote: 'No hay nada más peligroso que no arriesgarse',
+        player: 'Pep Guardiola'
+    },
+    {
+        quote: 'No hay presión cuando haces realidad un sueño',
+        player: 'Neymar'
+    },
+    {
+        quote: '¡Tu amor me hace fuerte, tu odio me hace imparable!',
+        player: 'Cristiano Ronaldo'
+    },
+    {
+        quote: 'El fútbol es un juego de errores. Quien cometa el menor error, gana',
+        player: 'Johan Cruyff'
+    },
+    {
+        quote: 'Siempre quiero más. Ya sea que se trate de un gol o de ganar un juego, nunca estoy satisfecho',
+        player: 'Messi'
+    },
+    {
+        quote: 'Todos los buenos atletas cometen errores; los grandes aprenden a cometer ese error solo una vez',
+        player: 'Raúl López'
+    },
+    {
+        quote: 'Tengo la oportunidad de ganarme la vida con lo que más me gusta en la vida. Puedo hacer feliz a la gente y disfrutar al mismo tiempo',
+        player: 'Ronaldo'
+    },
+    {
+        quote: 'Cuando la gente tiene éxito, es gracias al trabajo duro. La suerte no tiene nada que ver con el éxito',
+        player: 'Diego Maradona'
+    },
+    {
+        quote: 'No soy un dios, sólo soy un futbolista',
+        player: 'Zinedine Zidane'
+    },
+    {
+        quote: 'Cuanto más difícil es la victoria, mayor es la felicidad de ganar',
+        player: 'Pelé'
+    },
+    {
+        quote: 'El éxito sin honor es el mayor de los fracasos',
+        player: 'Vicente del Bosque'
+    },
+    {
+        quote: 'Algo en lo más profundo de mi persona me permite agarrar el éxito y seguir adelante tratando de ganar de nuevo',
+        player: 'Messi'
+    },
+    {
+        quote: 'El fútbol es simple, pero es difícil jugar de forma simple',
+        player: 'Johan Cruyff'
+    },
+    {
+        quote: 'Si muero un día, estoy feliz porque traté de dar lo mejor de mí. Mi deporte me permitió hacer mucho, porque es el deporte más grande del mundo',
+        player: 'Pelé'
+    },
+    {
+        quote: 'Odio perder y eso te da una determinación extra para trabajar más duro',
+        player: 'Wayne Rooney'  
+    },
+    {
+        quote: 'No tengo tiempo para pasatiempos. Al final del día, trato mi trabajo como un hobby. Es algo que me encanta hacer',
+        player: 'David Beckham'
+    },
+    {
+        quote: 'Convertirse en futbolista es solo la primera mitad de la oración silenciosa que un niño ofrece al cielo. La segunda parte es el nombre del equipo para el que quiere jugar',
+        player: 'Andrea Pirlo'
+    },
+    {
+        quote: 'Establecer un objetivo no es lo principal. Es decidir cómo lograrlo y seguir con ese plan',
+        player: 'Tom Landry'
+    },
+    {
+        quote: 'Una vez lloré porque no tenía zapatos para jugar fútbol, pero un día, conocí a un hombre que no tenía pies',
+        player: 'Zinedine Zidane'
+    },
+    {
+        quote: 'Siempre lancé la pelota, porque si recuperaba la pelota, era el único jugador sin marcar',
+        player: 'Johan Cruyff'
+    },
+    {
+        quote: 'No se trata solo del dinero, se trata de lo que se logra en el terreno de juego',
+        player: 'Ronaldinho'
+    },
+    {
+        quote: 'Ganar queremos todos, pero solo los mediocres no aspiran a la belleza. Es como pretender elegir entre un imbécil bueno o un inteligente malo',
+        player: 'Jorge Valdano'
+    },
+    {
+        quote: 'No podemos mirarnos siempre en el espejo y decir lo buenos que somos. Cuando las cosas van bien es cuando hay que estar más atentos. El miedo a perder es la razón fundamental para competir bien',
+        player: 'Pep Guardiola'
+    },
+    {
+        quote: 'Cada derrota es una victoria en sí misma',
+        player: 'Francisco Maturana'
+    },
+    {
+        quote: 'El fútbol se trata de tener el mejor juego ofensivo posible. Siempre me gusta jugar al fútbol ofensivo, y nadie me convencerá de lo contrario',
+        player: 'Johan Cruyff'
+    },
+    {
+        quote: 'Ganar, eso es lo más importante para mí. Es tan simple como eso',
+        player: 'Cristiano Ronaldo'
+    },
+    {
+        quote: 'Siiuuuuuuuu!!!',
+        player:  'Cristiano Ronaldo'
+    }
+]
