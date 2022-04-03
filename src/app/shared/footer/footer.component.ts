52fc0ffd7393ffba684affd4f21cfbe8a043280a
Andres2D <andres2d1997.1206@gmail.com>
2022-04-02 23:42:49 -0500

    Improve footer component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  OnDestroy, 
+  ChangeDetectionStrategy, 
+  ChangeDetectorRef 
+} from '@angular/core';
 import { quotes } from '../../constants/player-quotes';
 import { PlayerQuote } from '../../interfaces/player-quote.interface';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent implements OnInit, OnDestroy {
 
   interval: any;
   readonly quotes = quotes;
+  readonly environment = environment;
   quote: PlayerQuote = quotes[0];
-  constructor() { }
+
+  constructor(private cdr: ChangeDetectorRef) { }
 
   get quotesLenght() {
     return this.quotes.length - 1;
@@ -21,6 +31,7 @@ export class FooterComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
    this.interval = setInterval(() => {
       this.quote = this.quotes[Math.floor(Math.random() * this.quotesLenght)];
+      this.cdr.detectChanges();
     }, 30000);
   }
 
