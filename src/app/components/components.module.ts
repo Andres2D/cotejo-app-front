6bb02ccdb21b80def5916f6babe6d56de92da062
Andres2D <andres2d1997.1206@gmail.com>
2022-04-29 21:13:18 -0500

    Refactor auth components with material styles

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './alert/alert.component';
 import { CotejoButtonComponent } from './cotejo-button/cotejo-button.component';
 import { CotejoPlayerPreviewComponent } from './cotejo-player-preview/cotejo-player-preview.component';
@@ -7,7 +8,6 @@ import { CotejoRatingComponent } from './cotejo-rating/cotejo-rating.component';
 import { CotejoModalComponent } from './cotejo-modal/cotejo-modal.component';
 import { CotejoVersusComponent } from './cotejo-versus/cotejo-versus.component';
 import { CotejoFieldComponent } from './cotejo-field/cotejo-field.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { CotejoLoaderComponent } from './cotejo-loader/cotejo-loader.component';
 import { CotejoPickShieldColorComponent } from './cotejo-pick-shield-color/cotejo-pick-shield-color.component';
 import { CotejoPageCardsComponent } from './cotejo-page-cards/cotejo-page-cards.component';
@@ -43,6 +43,6 @@ import { CotejoTeamRatioStarsComponent } from './cotejo-team-ratio-stars/cotejo-
     CotejoPickShieldColorComponent,
     CotejoPageCardsComponent,
     CotejoTeamRatioStarsComponent
-  ]
+  ],
 })
 export class ComponentsModule { }
